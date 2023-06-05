4f7ee3c8f080ce3f74e28ede5ad07175897ff251
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2023-06-05 16:00:21 +0100

    Added Score List

--- PamFest/Assets/Scripts/GameManager.cs
+++ PamFest/Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,6 +9,7 @@ public class GameManager : MonoBehaviour
     public bool movingRight = true;
     public List<Player> players = new List<Player>();
     public int[] playersComplete = new int[4]; // 0 = Running, 1 = Completed, 2 = Eaten
+    public List<Player> winners = new List<Player>();
 
     public static GameManager instance;
 
@@ -27,11 +30,44 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // CHECK FOR THE END OF THE GAME
+        int playersLeft = players.Count;
+        for (int i = 0; i < players.Count; i++)
+        {
+            if (playersComplete[i] == 2)
+            {
+                playersLeft--;
+            }
+        }
+        if (playersLeft <= 1)
+        {
+            // end the game
+            print("Game Ended");
+            Time.timeScale = 0f;
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (!winners.Contains(players[i]))
+                {
+                    winners.Add(players[i]);
+                    break;
+                }
+            }
+        }
+
+
+
         bool finishedRound = true;
         for (int i = 0; i < players.Count; i++)
         {
-            if (playersComplete[i] > 0)
-            { 
+            if (playersComplete[i] == 2)
+            {
+                players[i].playerMovementEnabled = false;
+                if (!winners.Contains(players[i]))
+                    winners.Add(players[i]);
+                continue;
+            }
+            if (playersComplete[i] == 1)
+            {
                 players[i].playerMovementEnabled = false;
                 continue;
             }
