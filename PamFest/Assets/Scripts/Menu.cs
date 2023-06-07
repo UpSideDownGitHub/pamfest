6cc2017cdc0e49efe37293796b2362abdb026279
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2023-06-07 22:01:51 +0100

    Connect 2 Controllers

--- PamFest/Assets/Scripts/Menu.cs
+++ PamFest/Assets/Scripts/Menu.cs
@@ -2,14 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class Menu : MonoBehaviour
 {
-
+    public Animator anim;
 
     public void LoadScene(string sceneName)
     {
-
+        if (sceneName == "Loading")
+        {
+            // check to see if there are more than 1 controller connected
+            if (Gamepad.all.Count < 2)
+            {
+                anim.Play("New State 0");
+                // cant play the game as not enough controllers
+                // need to show a popup that says please connect another controller
+                return;
+            }
+        }
         StartCoroutine(SFXComp());
 
         IEnumerator SFXComp()
@@ -19,7 +30,7 @@ public class Menu : MonoBehaviour
         }
     }
 
-        public void QuitDaGame()
+    public void QuitDaGame()
     {
         Application.Quit();
     }
